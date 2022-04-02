a8df016115f47614710b38427cd2160ab4ecd7ba
Héctor Figueroa <69779567+Hejefica@users.noreply.github.com>
2022-04-01 20:23:09 -0600

    First Horse Race

--- 25HorsesSorting.py
+++ 25HorsesSorting.py
@@ -1,12 +1,26 @@
-from sqlite3 import Time
-
-
+import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 class Horse:
-    def __init__(self, N, T, UP, LP):
-        self.Name = N
+    def __init__(self, T, N = None, UP = None, LP = None):
         self.Time = T
+        self.Name = N
         self.UpperPosition = UP
         self.LowerPosition = LP
 
+def Random():
+    return f'{random.random():.4f}'
+
+gA = [Random(),Random(),Random(),Random(),Random()]
+gB = [Random(),Random(),Random(),Random(),Random()]
+gC = [Random(),Random(),Random(),Random(),Random()]
+gD = [Random(),Random(),Random(),Random(),Random()]
+print("25 horses into 5 groups:", "\n", gA, "\n", gB, "\n", gC, "\n", gD)
+
+#Race 1
+gA.sort()
+gB.sort()
+gC.sort()
+gD.sort()
+print("Group sort from slower to faster:", "\n", gA, "\n", gB, "\n", gC, "\n", gD)
